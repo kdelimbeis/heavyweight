8df60419e715d2c2f9da4afef090a9bf14cf2394
kostas delimbeis <kdelimbeis@comcast.net>
2017-08-07 12:30:07 -0400

    submit erb

--- app.rb
+++ app.rb
@@ -19,18 +19,27 @@ get '/inventory' do
   erb :inventory
 end
 
+get '/submit' do
+  erb :submit
+end
+
 post '/submit' do
   @name = params[:name]
 	@email = params[:email]
   from = Email.new(email: @email)
   to = Email.new(email: 'kdelimbeis@comcast.net')
-  subject = 'Thanks for contacting Heavyweight Auto Sales'
-  content = Content.new(type: 'text/plain', value: @name + ', we will reply within 2 business days.')
+  subject = 'Heavyweight customer inquiry'
+  content = Content.new(type: 'text/plain', value: 'Contact ' + @name + ' within 2 business days.')
   mail = Mail.new(from, subject, to, content)
 
   sg = SendGrid::API.new(api_key: ENV['SENDGRID_API_KEY'])
   response = sg.client.mail._('send').post(request_body: mail.to_json)
   puts response.status_code
 
-  redirect "/"
+  if response.status_code == '202'
+    redirect '/submit'
+  else
+    redirect '/contact'
+  end
+
 end
