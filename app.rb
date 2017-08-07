cbb8b2a9cc0c8ffa33baea62ce5dd2e57f100b28
kostas delimbeis <kdelimbeis@comcast.net>
2017-08-07 11:42:23 -0400

    css

--- app.rb
+++ app.rb
@@ -22,15 +22,15 @@ end
 post '/submit' do
   @name = params[:name]
 	@email = params[:email]
-from = Email.new(email: @email)
-to = Email.new(email: 'kdelimbeis@comcast.net')
-subject = 'Thanks for contacting Heavyweight Auto Sales'
-content = Content.new(type: 'text/plain', value: @name + ', we will reply within 2 business days.')
-mail = Mail.new(from, subject, to, content)
+  from = Email.new(email: @email)
+  to = Email.new(email: 'kdelimbeis@comcast.net')
+  subject = 'Thanks for contacting Heavyweight Auto Sales'
+  content = Content.new(type: 'text/plain', value: @name + ', we will reply within 2 business days.')
+  mail = Mail.new(from, subject, to, content)
 
-sg = SendGrid::API.new(api_key: ENV['SENDGRID_API_KEY'])
-response = sg.client.mail._('send').post(request_body: mail.to_json)
-puts response.status_code
-puts response.body
-puts response.headers
+  sg = SendGrid::API.new(api_key: ENV['SENDGRID_API_KEY'])
+  response = sg.client.mail._('send').post(request_body: mail.to_json)
+  puts response.status_code
+
+  redirect "/"
 end
