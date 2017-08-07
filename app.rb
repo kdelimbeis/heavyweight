b36b132f2d642eac6193d4b9f5f17db9e747a5be
kostas delimbeis <kdelimbeis@comcast.net>
2017-08-06 22:19:09 -0400

    contact

--- app.rb
+++ app.rb
@@ -1,5 +1,7 @@
 require 'sinatra'
+require 'sendgrid-ruby'
 
+include SendGrid
 
 get '/' do
   erb :index
@@ -15,4 +17,20 @@ end
 
 get '/inventory' do
   erb :inventory
+end
+
+post '/submit' do
+  @name = params[:name]
+	@email = params[:email]
+from = Email.new(email: @email)
+to = Email.new(email: 'kdelimbeis@comcast.net')
+subject = 'Thanks for contacting Heavyweight Auto Sales'
+content = Content.new(type: 'text/plain', value: @name + ', we will reply within 2 business days.')
+mail = Mail.new(from, subject, to, content)
+
+sg = SendGrid::API.new(api_key: ENV['SENDGRID_API_KEY'])
+response = sg.client.mail._('send').post(request_body: mail.to_json)
+puts response.status_code
+puts response.body
+puts response.headers
 end
